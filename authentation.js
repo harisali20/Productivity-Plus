63b03b90c704ca323b4d336bd64796ca4e7c9cd8
Ahmed abdulkareem <ahmedabdalkaremm@gmail.com>
2024-05-18 20:39:44 +0500

    Authentication is working fire by Ahmed baby

--- authentation.js
+++ authentation.js
@@ -1,64 +1,64 @@
 
-alert('something');
-
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/database.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.0/database-auth.js";
-
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDqf8tTg_jd2JLLYspMfhNbjp3FvJw4Q2M",
-  authDomain: "pplus-66907.firebaseapp.com",
-  databaseURL: "https://pplus-66907-default-rtdb.firebaseio.com",
-  projectId: "pplus-66907",
-  storageBucket: "pplus-66907.appspot.com",
-  messagingSenderId: "350615844207",
-  appId: "1:350615844207:web:50b2973a4650b5a65f649a",
-  measurementId: "G-EYRXZT1MQC"
+  apiKey: "AIzaSyDFzMK5H7Gd_yipswUle9jnZWFGLeYxhY0",
+  authDomain: "proplus-aa523.firebaseapp.com",
+  projectId: "proplus-aa523",
+  storageBucket: "proplus-aa523.appspot.com",
+  messagingSenderId: "701524565838",
+  appId: "1:701524565838:web:f5bd2ee58cd11c4c26481f"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-const analytics = getAnalytics(app);
-const auth = getAuth();
+const auth = getAuth(app);
 
+// For the Sign In button
+const signInSubmit = document.getElementById('signIn');
+signInSubmit.addEventListener("click", function(event){
+  event.preventDefault();
 
+  // inputs
+  const email = document.getElementById('signInEmail').value;
+  const password = document.getElementById('signInPassword').value;
 
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      alert("Logged in successfully");
+      window.location.href = "index.app.html";
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage);
+    });
+});
 
+// For the Sign Up button
+const signUpSubmit = document.getElementById('signUp');
+signUpSubmit.addEventListener("click", function(event){
+  event.preventDefault();
 
-
-//for the sign up button
-signUp-BigInt.addEventListener('click', (e)=>{
-
-  var email = document.getElementById('signUpEmail').value;
-  var password = document.getElementById('signUpPassword').value;
-  var username = document.getElementById('signUpName').value;
-
-
+  // inputs
+  const email = document.getElementById('signUpEmail').value;
+  const password = document.getElementById('signUpPassword').value;
 
   createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed up 
-    const user = userCredential.user;
-    alert('userCreated');
-
-    // ...Here you will be navigated to the next page.
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-
-    alert(errorMessage);
-    // ..
-  });
-
+    .then((userCredential) => {
+      // Signed up
+      const user = userCredential.user;
+      alert("Account created successfully");
+      window.location.href = "index.app.html";
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage);
+    });
 });
